03e4a3d05522f95e3f79ad0148a39f73999d7882
Adriana <dri.bartolini@gmail.com>
2021-04-13 21:01:48 -0300

    estrutura familia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FamiliesComponent } from './modules/customers/views/families/families.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FamiliesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/customers/customers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FamiliesComponent } from './views/families/families.component';
+
+
+
+@NgModule({
+  declarations: [
+    FamiliesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomersModule { }
